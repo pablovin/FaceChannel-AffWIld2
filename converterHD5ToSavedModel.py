f1045c503e3d0f2f3411eaa61c48d62330121ae6
Pablo <pablo.alvesdebarros@iit.it>
2020-09-18 23:09:07 +0200

    Update for gcloud - all frames!

--- converterHD5ToSavedModel.py
+++ converterHD5ToSavedModel.py
@@ -8,6 +8,9 @@ from tensorflow.python.saved_model.signature_def_utils_impl import predict_signa
 def to_savedmodel(model, export_path):
   """Convert the Keras HDF5 model into TensorFlow SavedModel."""
 
+  from keras.models import load_model
+  model = load_model(model)
+
   builder = saved_model_builder.SavedModelBuilder(export_path)
 
   signature = predict_signature_def(inputs={'input': model.inputs[0]},
